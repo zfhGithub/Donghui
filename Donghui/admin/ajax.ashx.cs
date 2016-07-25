235bb3df71058ae933b4dc3b5527782a985bc33c
377108616@qq.com <a1314520.>
2016-07-26 00:04:25 +0800

    2016-07-26 00:04 家里

--- Donghui/admin/ajax.ashx.cs
+++ Donghui/admin/ajax.ashx.cs
@@ -22,8 +22,9 @@ namespace Donghui.admin
 
             switch (action)
             {
+                #region 关于我们
                 case "getaboutinfo":
-                    res.Write(Utils.DataTableToJSON( com.aboutinfo.getAboutInfo()));
+                    res.Write(Utils.DataTableToJSON(com.aboutinfo.getAboutInfo()));
                     break;
                 case "setaboutus":
                     string aboutusText = req.Form["aboutus"];
@@ -31,7 +32,7 @@ namespace Donghui.admin
                     context.Response.Write(Utils.GetReulst(200, "设置成功", "设置失败", status));
                     break;
                 case "setachievement":
-                    string achievementText = req.Form["achievement"]; 
+                    string achievementText = req.Form["achievement"];
                     status = com.aboutinfo.setAboutInfo(achievementText: achievementText);
                     context.Response.Write(Utils.GetReulst(200, "设置成功", "设置失败", status));
                     break;
@@ -44,10 +45,22 @@ namespace Donghui.admin
                     HttpPostedFile file = req.Files[0];
                     res.Write(Utils.UploadImage(file, req.QueryString["type"]));
                     break;
-                case "setcontact": 
+                case "setcontact":
                     status = com.aboutinfo.setContact(req.Form["cname"], req.Form["caddress"], req.Form["cemail"], req.Form["cphone"]);
                     context.Response.Write(Utils.GetReulst(200, "设置成功", "设置失败", status));
                     break;
+                #endregion
+
+                case "messagelist":
+                    string currentIndex = req.Form["pageIndex"];
+                    string pageCount = req.Form["pageSize"];
+                    Dictionary<string, string> jsonDic = new Dictionary<string, string>();
+                    jsonDic.Add("data", Utils.DataTableToJSON( com.message.getMessageList(currentIndex, pageCount)));
+                    jsonDic.Add("count", com.message.getMessageListCount());
+                    res.Write();
+                    break;
+                       
+
             }
         }
 
